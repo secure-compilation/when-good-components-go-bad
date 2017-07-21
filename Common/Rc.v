4d503b4d413dbeb9806a0f380aa5b43beebd8d61
Marco Stronati <marco@stronati.org>
2017-07-21 21:08:13 +0200

    Reorganized modules.

--- Common/Rc.v
+++ Common/Rc.v
@@ -8,7 +8,11 @@ Require Import Coq.Program.Basics.
    - target decomposition,
    - compiler correctness and 
    - source definability
+  
+   For a simple instance refer to SI.RC, a more complicate instance is
+   in Main.robust_compilation_static_compromise.
  *)
+
 
 (* 
    Some Global definitions.
@@ -113,9 +117,9 @@ Module T <: Lang.
   Axiom partialize: interface -> T.program -> option T.program.
   Axiom partialize_post:
     forall (p:T.program) psi,
-      exists pp, 
-        (T.get_interface p) = (T.get_interface pp)++psi ->
-        partialize psi p = Some pp.
+    exists pp, 
+      (T.get_interface p) = (T.get_interface pp)++psi ->
+      partialize psi p = Some pp.
   Definition opartialize psi (op: option program) := propagate (partialize psi) op.
 End T.
 
@@ -148,7 +152,7 @@ Module I <: Lang.
       complete2 c p ->
       behaves (osem (link c p)) beh ->
       (turn beh (get_interface p) ->
-      not_wrong beh).
+       not_wrong beh).
 End I.
 
 (* Source *)
@@ -170,7 +174,7 @@ Module S <: Lang.
       complete2 c p ->
       behaves (osem (link c p)) beh ->
       (turn beh (get_interface p) ->
-      not_wrong beh).
+       not_wrong beh).
 
   Axiom definability:
     forall (beh:program_behavior) (psi:interface) (p:program),
@@ -182,7 +186,9 @@ End S.
 
 
 (* This module takes a high language L1 and a low one L2 and produces
-   a compiler from L1 to L2 and some properties *)
+   a compiler from L1 to L2 and some properties.
+   This is common between the backends, MP SFI, and the SI compiler.
+ *)
 Module Compiler (L1 L2: Lang).
   Axiom compile : L1.program -> L2.program.
   Axiom compile_complete :
@@ -190,65 +196,107 @@ Module Compiler (L1 L2: Lang).
       L1.complete p1 -> compile p1 = p2 -> L2.complete p2.
   
   Axiom compiler_interfaces:
-  forall (p1:L1.program),
-    L1.get_interface p1 = L2.get_interface (compile p1).
+    forall (p1:L1.program),
+      L1.get_interface p1 = L2.get_interface (compile p1).
 End Compiler.
 
-Module IT := Compiler I T.
-Module SI := Compiler S I.
+(* Source to Intermediate *)
+Module SI.
+  Include Compiler S I.
 
-(* TODO IT.compile must be a refinement compiler *)
+  (* exact cc preserves also UB *)
+  Axiom compiler_correctness :
+    forall beh (p:S.program) (psi:interface),
+      behaves (I.psem psi (SI.compile p)) beh ->
+      behaves (S.psem psi p) beh.
 
-(* exact cc preserves also UB *)
-Axiom SIcompiler_correctness :
-  forall beh (p:S.program) (psi:interface),
-    behaves (I.psem psi (SI.compile p)) beh ->
-    behaves (S.psem psi p) beh.
+  (* TODO This is too syntactic, it should be stated in terms of equivalent behaviors *)
+  (* Only needed for RC variant *)
+  Axiom separate_compilation:
+    forall (P Q: S.program),
+      map SI.compile (S.link Q P) = I.link (SI.compile Q) (SI.compile P).
 
+  (* RC between S and I *)
+  Definition RC:
+    forall (c:I.program) (P:S.program) (beh:program_behavior),
+      icomplete2 (I.get_interface c) (S.get_interface P) ->
+      S.fully_defined (I.get_interface c) P ->
+      behaves (I.osem (I.link c (SI.compile P))) beh
+      ->
+      exists C,
+        behaves (S.osem (S.link C P)) beh /\ S.complete2 C P.
+  Proof.
+    intros c P b Hcomplete SFD H.
+    apply I.decomposition in H.
+    destruct (I.link_prepost c (SI.compile P)) as [p [H1 H2]].
+    unfold I.complete2.
+    rewrite <- SI.compiler_interfaces.
+    auto.
+    apply (SI.compiler_correctness b P (I.get_interface c)) in H.
+    apply S.definability in H.
+    destruct H as [C [Hb Hif]].
+    exists C.
+    split.
+    auto.
+    rewrite <- Hif in Hcomplete.
+    auto.
+    auto.
+  Qed.
 
-(* Warm-up: RC between S and I *)
-Definition SIrc:
-  forall (c:I.program) (P:S.program) (beh:program_behavior),
-    icomplete2 (I.get_interface c) (S.get_interface P) ->
-    S.fully_defined (I.get_interface c) P ->
-    behaves (I.osem (I.link c (SI.compile P))) beh
-    ->
-    exists C,
-      behaves (S.osem (S.link C P)) beh /\ S.complete2 C P.
-Proof.
-  intros c P b Hcomplete SFD H.
-  apply I.decomposition in H.
-  destruct (I.link_prepost c (SI.compile P)) as [p [H1 H2]].
-  unfold I.complete2.
-  rewrite <- SI.compiler_interfaces.
-  auto.
-  apply (SIcompiler_correctness b P (I.get_interface c)) in H.
-  apply S.definability in H.
-  destruct H as [C [Hb Hif]].
-  exists C.
-  split.
-  auto.
-  rewrite <- Hif in Hcomplete.
-  auto.
-  auto.
-Qed.
+  (* This is proved using RC. *)
+  Definition FD_preservation:
+    forall (psi:interface) (p:S.program),
+      S.fully_defined psi p ->
+      I.fully_defined psi (SI.compile p).
+  Proof.
+    intros psi p SFD.
+    intros b c Hpsi Hcomplete H1.
+    unfold I.complete2 in Hcomplete.
+    rewrite <- SI.compiler_interfaces in Hcomplete.
+    destruct (SI.RC c p b Hcomplete) as [C [H2 H3]].
+    rewrite Hpsi.
+    apply SFD.
+    assumption.
+    unfold S.fully_defined in SFD.
+    rewrite <- SI.compiler_interfaces.
+    apply (SFD b C).
+    unfold S.complete2 in H3.  
+    destruct (unique_interfaces (I.get_interface c) (S.get_interface C) (S.get_interface p)); auto.
+    auto.
+    auto.
+  Qed.
+End SI.
 
+(* Both backend MP and SFI need to implement this interface *)
+Module Type IT.
+  (* TODO IT.compile must be a refinement compiler *)
+  Axiom compile : I.program -> T.program.
+  
+  (* 
+     We now got for RC between S and T.
+     The following properties are special because they depend on
+     compiling the complete intermediate program.
+   *)
+  Axiom special_decomposition :
+    forall beh (c p:I.program),
+      I.fully_defined (I.get_interface c) p ->
+      let ip := map IT.compile (I.link c p) in
+      behaves (T.osem ip) beh
+      ->
+      behaves (T.opsem (I.get_interface c) (T.opartialize (I.get_interface c) ip)) beh.
 
-(* 
-   We now got for RC between S and T.
-   The following properties are special because they depend on
-   compiling the complete intermediate program.
- *)
-Definition Tspecial_decomposition :=
-  forall beh (c p:I.program),
-    I.fully_defined (I.get_interface c) p ->
-    let ip := map IT.compile (I.link c p) in
-    behaves (T.osem ip) beh
-    ->
-    behaves (T.opsem (I.get_interface c) (T.opartialize (I.get_interface c) ip)) beh.
+  Axiom special_compiler_correctness:
+    forall beh (c p:I.program),
+      I.fully_defined (I.get_interface c) p ->
+      let ip := I.link c p in
+      let tp := T.opartialize (I.get_interface c) (map compile ip) in
+      behaves (T.opsem (I.get_interface c) tp) beh ->
+      behaves (I.opsem (I.get_interface c) (Some p))  beh.
+End IT.
 
-Module MP.
-  Include T.
+(* Micro-policies backend *)
+Module MP <: IT.
+  Include Compiler I T.
 
   Axiom decomposition:
     forall beh (p:T.program) psi,
@@ -258,7 +306,13 @@ Module MP.
 
   (* we can prove special decomposition using the more general
      decomposition *)
-  Definition special_decomposition : Tspecial_decomposition.
+  Definition special_decomposition :
+    forall beh (c p:I.program),
+      I.fully_defined (I.get_interface c) p ->
+      let ip := map compile (I.link c p) in
+      behaves (T.osem ip) beh
+      ->
+      behaves (T.opsem (I.get_interface c) (T.opartialize (I.get_interface c) ip)) beh.
   Proof.
     intros b c p IFD ip H.
     destruct ip.
@@ -267,102 +321,101 @@ Module MP.
     simpl in H.
     auto.
   Qed.
+
+  Axiom special_compiler_correctness:
+    forall beh (c p:I.program),
+      I.fully_defined (I.get_interface c) p ->
+      let ip := I.link c p in
+      let tp := T.opartialize (I.get_interface c) (map compile ip) in
+      behaves (T.opsem (I.get_interface c) tp) beh ->
+      behaves (I.opsem (I.get_interface c) (Some p))  beh.
 End MP.
 
+(* Software Fault Isolation backend *)
+Module SFI <: IT.
+  Include Compiler I T.
+
+  (* there is no generic decomposition, we need to prove
+     special_decomposition *)
+  Axiom special_decomposition :
+    forall beh (c p:I.program),
+      I.fully_defined (I.get_interface c) p ->
+      let ip := map compile (I.link c p) in
+      behaves (T.osem ip) beh
+      ->
+      behaves (T.opsem (I.get_interface c) (T.opartialize (I.get_interface c) ip)) beh.
+
+  Axiom special_compiler_correctness:
+    forall beh (c p:I.program),
+      I.fully_defined (I.get_interface c) p ->
+      let ip := I.link c p in
+      let tp := T.opartialize (I.get_interface c) (map compile ip) in
+      behaves (T.opsem (I.get_interface c) tp) beh ->
+      behaves (I.opsem (I.get_interface c) (Some p))  beh.
+End SFI.
 
 
 
-Axiom ITspecial_compiler_correctness:
-  forall beh (c p:I.program),
-    I.fully_defined (I.get_interface c) p ->
-    let ip := I.link c p in
-    let tp := T.opartialize (I.get_interface c) (map IT.compile ip) in
-    behaves (T.opsem (I.get_interface c) tp) beh ->
-    behaves (I.opsem (I.get_interface c) (Some p))  beh.
 
-(* This is proved using SIrc. *)
-Definition FD_preservation:
-  forall (psi:interface) (p:S.program),
-  S.fully_defined psi p ->
-  I.fully_defined psi (SI.compile p).
-Proof.
-  intros psi p SFD.
-  intros b c Hpsi Hcomplete H1.
-  unfold I.complete2 in Hcomplete.
-  rewrite <- SI.compiler_interfaces in Hcomplete.
-  destruct (SIrc c p b Hcomplete) as [C [H2 H3]].
-  rewrite Hpsi.
-  apply SFD.
-assumption.
-  unfold S.fully_defined in SFD.
-  rewrite <- SI.compiler_interfaces.
-  apply (SFD b C).
-  unfold S.complete2 in H3.  
-  destruct (unique_interfaces (I.get_interface c) (S.get_interface C) (S.get_interface p)); auto.
-auto.
-auto.
-Qed.
-
-(* 
+(* The proof in modular wrt the backend *)
+Module Main (IT: IT).
+  (* 
    This property is different from the one we started from: there is
    no concept of linking at the low level.
    A program is composed at the intermediate, compiled as a complete
    program and run at the target.
    Instead of linking to partial target programs, we go to the partial
    semantics by partializing a complete target program.
- *)
+   *)
 
-Definition robust_compilation_static_compromise :=
-  forall (c:I.program) (P:S.program) (beh:program_behavior),
-    icomplete2 (I.get_interface c) (S.get_interface P) ->
-    S.fully_defined (I.get_interface c) P ->
-    behaves (T.osem (map IT.compile (I.link c (SI.compile P)))) beh
-    ->
-    exists C, 
-      behaves (S.osem (S.link C P)) beh /\ S.complete2 C P.
+  Definition robust_compilation_static_compromise :=
+    forall (c:I.program) (P:S.program) (beh:program_behavior),
+      icomplete2 (I.get_interface c) (S.get_interface P) ->
+      S.fully_defined (I.get_interface c) P ->
+      behaves (T.osem (map IT.compile (I.link c (SI.compile P)))) beh
+      ->
+      exists C, 
+        behaves (S.osem (S.link C P)) beh /\ S.complete2 C P.
 
-Theorem proof_rc_static : robust_compilation_static_compromise.
-Proof.
-  intros c P b Hcomplete SFD H.
-  apply (MP.special_decomposition b c (SI.compile P)) in H.
-  apply ITspecial_compiler_correctness in H.
-  destruct (S.definability b (I.get_interface c) P Hcomplete) as [C [H1 Hinterfaces]].
-  apply (SIcompiler_correctness b P (I.get_interface c)) in H.
-  assumption.
-  rewrite <- Hinterfaces in Hcomplete.
-  exists C.
-  auto.
-  apply FD_preservation.
-  assumption.
-  apply FD_preservation.
-  assumption.
-Qed.
+  Theorem proof_rc_static : robust_compilation_static_compromise.
+  Proof.
+    intros c P b Hcomplete SFD H.
+    apply (IT.special_decomposition b c (SI.compile P)) in H.
+    apply IT.special_compiler_correctness in H.
+    destruct (S.definability b (I.get_interface c) P Hcomplete) as [C [H1 Hinterfaces]].
+    apply (SI.compiler_correctness b P (I.get_interface c)) in H.
+    assumption.
+    rewrite <- Hinterfaces in Hcomplete.
+    exists C.
+    auto.
+    apply SI.FD_preservation.
+    assumption.
+    apply SI.FD_preservation.
+    assumption.
+  Qed.
 
 
-(* This definition is weaker than the above but has the advantage of
+  (* This definition is weaker than the above but has the advantage of
    not mentioning the intermediate language *)
-Definition robust_compilation_static_compromise_corollary :=
-  forall (Q P:S.program) (beh:program_behavior),
-    S.complete2 Q P ->
-    S.fully_defined (S.get_interface Q) P ->
-    let compile := compose IT.compile SI.compile in
-    behaves (T.osem (map compile (S.link Q P))) beh ->
-    exists C, behaves (S.osem (S.link C P)) beh /\ S.complete2 C P.
+  Definition robust_compilation_static_compromise_corollary :=
+    forall (Q P:S.program) (beh:program_behavior),
+      S.complete2 Q P ->
+      S.fully_defined (S.get_interface Q) P ->
+      let compile := compose IT.compile SI.compile in
+      behaves (T.osem (map compile (S.link Q P))) beh ->
+      exists C, behaves (S.osem (S.link C P)) beh /\ S.complete2 C P.
 
-(* TODO This is too syntactic, it should be stated in terms of equivalent behaviors *)
-Axiom Sseparate_compilation:
-  forall (P Q: S.program),
-    map SI.compile (S.link Q P) = I.link (SI.compile Q) (SI.compile P).
+  Corollary robust_compilation_corrolary :
+    robust_compilation_static_compromise ->
+    robust_compilation_static_compromise_corollary.
+  Proof.
+    intros RC Q P b Hcompl H1 H2 H3.
+    specialize (RC (SI.compile Q) P b).
+    rewrite <- SI.compiler_interfaces in RC.
+    unfold H2 in H3.
+    rewrite <- SI.separate_compilation in RC.
+    rewrite <- map_compose in H3.  
+    apply (RC Hcompl H1 H3).
+  Qed.
 
-Corollary robust_compilation_corrolary :
-  robust_compilation_static_compromise ->
-  robust_compilation_static_compromise_corollary.
-Proof.
-  intros RC Q P b Hcompl H1 H2 H3.
-  specialize (RC (SI.compile Q) P b).
-  rewrite <- SI.compiler_interfaces in RC.
-  unfold H2 in H3.
-  rewrite <- Sseparate_compilation in RC.
-  rewrite <- map_compose in H3.  
-  apply (RC Hcompl H1 H3).
-Qed.
+End Main.
