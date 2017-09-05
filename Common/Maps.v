eb6787b7ae090a7bdd33cc642cf02c2308112c07
Ana Nora Evans <ananevans@gmail.com>
2017-09-04 22:51:46 -0400

    added map with keys of type N

--- Common/Maps.v
+++ Common/Maps.v
@@ -7,3 +7,8 @@ Module backNat_as_OT := Backport_OT Nat_as_OT.
 Module NMap := FMapAVL.Make backNat_as_OT.
 Module NMapExtra := WProperties_fun Nat_as_OT NMap.
 Module NMapFacts := NMapExtra.F.
+
+Module N_as_OT := Backport_OT N_as_OT.
+Module BinNatMap := FMapAVL.Make(N_as_OT).
+Module BinNatMapExtra := WProperties_fun N_as_OT BinNatMap.
+Module BinNatMapFacts := BinNatMapExtra.F.
