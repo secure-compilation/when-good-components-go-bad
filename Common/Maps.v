bde160048c34c89250ab4ef2abc64279eafc3fdf
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2017-12-12 14:31:17 -0500

    Update CoqUtils.

--- Common/Maps.v
+++ Common/Maps.v
@@ -1,6 +1,6 @@
 From CoqUtils Require Export ord fmap fset.
 From mathcomp Require Export ssrbool.
 
-Definition NMap := FMap.fmap_type nat_ordType.
+Definition NMap T := {fmap nat -> T}.
 
-Definition elementsm {A: Type} : @NMap A -> list (nat * A) := @FMap.fmval nat_ordType A.
+Definition elementsm {A: Type} : NMap A -> list (nat * A) := @FMap.fmval nat_ordType A _.
