96c94110aba781932fed07c1b6883dc842736654
Theo Laurent <theo.laurent@ens.fr>
2018-01-09 15:26:34 +0100

    WIP

---
+++ I2MP/Compiler.v
@@ -0,0 +1,8 @@
+
+Require Import Intermediate.Machine.
+Require Import MicroPolicies.Symbolic.
+
+
+
+
+Definition compile (p : Intermediate.program) : unit.
--- MicroPolicies/Instance.v
+++ MicroPolicies/Instance.v
@@ -1,14 +1,16 @@
-
+From mathcomp Require Import ssreflect eqtype.
 
 Require Import Symbolic Exec.
 Require Import Int32.
+Require Export LRC. (* TL TODO: that's not very clean *)
 
 (* TL TODO: look at how to instanciate a module parameter!! *)
 
 Definition mt := concrete_int_32_mt.
 
-Definition sp : Symbolic.params.
-Admitted.
+Definition sp : Symbolic.params :=
+  {| Symbolic.ttypes := lrc_tags ; Symbolic.transfer := transfer ; Symbolic.internal_state := [eqType of unit] |}
+.
 
 Definition table : @Symbolic.syscall_table mt sp.
 Admitted.
