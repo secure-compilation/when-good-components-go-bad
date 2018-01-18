d8dec3636dbb0030d692312546e7eb517a562967
Ana Nora Evans <ananevans@gmail.com>
2018-01-18 08:42:40 -0500

    changed order

--- bin/run_injection.sh
+++ bin/run_injection.sh
@@ -6,16 +6,17 @@ mkdir -p ../test_out
 
 export RAND_SEED=$RANDOM
 
-TESTS=(store jump stack correct)
-FRECV=(undef def spec)
+TESTS=(jump stack store)
+FRECV=(spec undef def)
 FLAGS=(store store1 store2 jump jump1 jump2 push pop call targets)
 
-for t in ${TESTS[*]};
-do
-    for f in ${FRECV[*]};
-    do    
-	for fl in ${FLAGS[*]};
-	do
+
+for f in ${FRECV[*]};
+    do
+        for fl in ${FLAGS[*]};
+        do
+		for t in ${TESTS[*]};
+		do
 		echo "Run $t $f ${fl}"
 		./run_test $t $f ${fl} > ../test_out/${t}_${f}_${fl}`date +"%b%d_%H_%M_%S"`
 	done
