62d9eafa9bd90f051f3de00969f967cac52bdb04
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2018-01-31 16:54:28 -0500

    Auxiliary lemmas.

--- Lib/Extra.v
+++ Lib/Extra.v
@@ -1,7 +1,29 @@
-(* Complements to the standard Coq library and ssreflect *)
+(* Complements to the standard Coq library, ssreflect, and Coq utils *)
 
 Require Import Coq.ZArith.ZArith.
-From mathcomp Require Import ssreflect ssrfun ssrbool eqtype.
+From mathcomp Require Import ssreflect ssrfun ssrbool eqtype seq.
+From CoqUtils Require Import ord fset fmap.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
 
 Definition Z_eqMixin := EqMixin Z.eqb_spec.
 Canonical Z_eqType := Eval hnf in EqType Z Z_eqMixin.
+
+Section FMap.
+
+Variables (T : ordType) (S S' : Type).
+Implicit Types (m : {fmap T -> S}) (x : T).
+
+(* FIXME: Move to CoqUtils *)
+
+Lemma domm_mapi (f : T -> S -> S') m : domm (mapim f m) = domm m.
+Proof.
+by apply/eq_fset=> x; rewrite !mem_domm mapimE; case: (m x).
+Qed.
+
+Lemma domm_map (f : S -> S') m : domm (mapm f m) = domm m.
+Proof. exact: domm_mapi. Qed.
+
+End FMap.
