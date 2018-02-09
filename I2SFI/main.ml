a9d47163977a6d860e240655ca32a6e4da8f80d8
Ana Nora Evans <ananevans@gmail.com>
2018-02-09 13:26:28 -0500

    small changes

--- I2SFI/main.ml
+++ I2SFI/main.ml
@@ -1,12 +1,14 @@
-Open Run_test
+open Run_test
 
 let run run_test : unit =
 begin
-    List.fold_left (fun _ c -> print_char c) () (run_test ct ig cf);
+    List.fold_left (fun _ c -> print_char c) () (run_test);
     print_newline ()
 end
 
 let main() = begin
+print_string "Run store test\n";
+print_newline ();
 run run_store_test;
 run run_jump_test;
 run run_stack_test;
--- run_sfi_tests.sh
+++ run_sfi_tests.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-ocaml /tmp/run_store_test.ml
-ocaml /tmp/run_jump_test.ml
-ocaml /tmp/run_stack_test.ml
+if [ -e "sfi_safety_properties.exe" ]
+then
+	./sfi_safety_properties.exe
+else
+	make -f Makefile.Test
+	if [ "$?" -eq "0" ]
+	then
+		echo "========================"
+		echo "Running Tests"
+		./sfi_safety_properties.exe
+	fi
+fi
