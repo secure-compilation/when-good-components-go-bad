54f0a6934141f0b19c3fb9b6f5f2ce3d489a8922
Jérémy Thibault <jeremy.thibault@ens-rennes.fr>
2018-03-01 11:36:09 +0100

    Add a new simple example and appropriate helper functions
    Test.v contains the new example, written in the intermediate language, that simply writes a number in the register 2

--- MicroPolicies/Examples/Helper.v
+++ MicroPolicies/Examples/Helper.v
@@ -4,6 +4,7 @@ From CoqUtils Require Import fmap fset word.
 Require Import Common.Definitions.
 Require Import Common.Values.
 Require Import Source.Language.
+Require Import Intermediate.Machine.
 Require Import S2I.Compiler.
 Require Import I2MP.Encode.
 Require Import I2MP.Precompile.
@@ -19,12 +20,21 @@ Fixpoint execN (n: nat) (st: state) : option state :=
   | O => None
   | S n' =>
     match stepf st with
-    | None => Some st
+    | None => None
     | Some st' => execN n' st'
     end
   end.
 
-Definition reg0 : {fmap reg Symbolic.mt -> ratom } := emptym.
+(* Six needed registers *)
+Definition reg0 : {fmap reg Symbolic.mt -> ratom } :=
+  let m := emptym in
+  let m := setm m (as_word 1) (Atom (as_word 0) (Other)) in
+  let m := setm m (as_word 2) (Atom (as_word 0) (Other)) in
+  let m := setm m (as_word 3) (Atom (as_word 0) (Other)) in
+  let m := setm m (as_word 4) (Atom (as_word 0) (Other)) in
+  let m := setm m (as_word 5) (Atom (as_word 0) (Other)) in
+   setm m (as_word 6) (Atom (as_word 0) (Other)).
+
 
 Definition load (m : {fmap mword Symbolic.mt -> matom }) : state :=
   {| Symbolic.mem := m ;
@@ -49,4 +59,10 @@ Definition compile_and_run (p: Source.program) (fuel: nat) :=
     end
   end.
 
-Compute compile_program identity.
+Definition compile_and_run' (p: Intermediate.program) (fuel:nat) :=
+  let st := load (encode (precompile p)) in
+    match execN fuel st with
+    | None => print_error ocaml_int_1
+    | Some st' => print_reg st' 2
+    end
+.
