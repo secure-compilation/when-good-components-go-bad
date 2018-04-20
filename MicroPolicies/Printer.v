9bbc38b1bf91ee59dfb568f8261789f4e9a92b03
Theo Laurent <theo.laurent@ens.fr>
2018-04-20 11:35:42 +0200

    A MicroPolicy machine state pretty(?)-printer.

--- MicroPolicies/Printer.v
+++ MicroPolicies/Printer.v
@@ -15,12 +15,46 @@ Axiom coqstring_of_word : forall {k}, word k -> string.
 (* TL TODO: move this to Extraction.v *)
 Extract Constant coqstring_of_word => "(fun _ w -> let Word x = Lazy.force w in coqstring_of_camlstring (Big.to_string x))".
 
-Definition coqstring_of_ratom (a : ratom) : string := "TODO:ratom".
 
-Definition coqstring_of_reg (r : reg mt * ratom) : string :=
-  "reg " ++ coqstring_of_word (fst r) ++ " : " ++ coqstring_of_ratom (snd r).
+Definition coqstring_of_value_tag (t : value_tag) : string :=
+  match t with
+    | Ret n => "Ret [TODO]"
+    | Other => "Other"
+  end.
+
+Definition coqstring_of_ratom (a : ratom) : string :=
+  "{ value: " ++ coqstring_of_word (vala a) ++ "; " ++ "tag: " ++ coqstring_of_value_tag (taga a) ++ " }".
 
 Definition coqstring_of_regs (regs : { fmap reg mt -> ratom }) : string :=
-  "{
-" ++ foldl (fun s r => coqstring_of_reg r ++ "
-" ++ s) "" regs ++ "}".
+  "regs:{
+" ++ foldl (fun s r =>
+              "reg " ++ coqstring_of_word (fst r) ++ " : " ++ coqstring_of_ratom (snd r) ++ "
+" ++ s) "" regs ++ "}
+".
+
+
+Definition coqstring_of_matom (a : matom) : string :=
+  "{ value: " ++ coqstring_of_word (vala a) ++ "; " ++ "tag: " ++ "TODO" ++ " }".
+
+Definition coqstring_of_mem (mem : { fmap mword mt -> matom }) : string :=
+  "mem:{
+" ++ foldl (fun s m =>
+              coqstring_of_word (fst m) ++ " : " ++ coqstring_of_matom (snd m) ++ "
+" ++ s) "" mem ++ "}
+".
+
+Definition coqstring_of_pc (pc : atom (mword mt) pc_tag) : string :=
+  "pc: " ++ coqstring_of_word (vala pc) ++ "; " ++ "TODO" ++ "
+".
+
+Definition coqstring_of_internal (_ : unit) : string := "".
+
+
+Definition coqstring_of_state (st : state) : string :=
+"============================
+" ++ coqstring_of_pc (Symbolic.pc st)
+  ++ coqstring_of_internal (Symbolic.internal st)
+  ++ coqstring_of_regs (Symbolic.regs st)
+  ++ coqstring_of_mem (Symbolic.mem st) ++
+"============================
+".
