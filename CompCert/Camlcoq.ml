53b773222500902104f72297e2fc7be365157efc
Theo Laurent <theo.laurent@ens.fr>
2018-05-08 11:23:31 +0200

    No need for license statement, it is in the directory.

--- CompCert/Camlcoq.ml
+++ CompCert/Camlcoq.ml
@@ -1,22 +1,5 @@
-(* *********************************************************************)
-(*                                                                     *)
-(*              The Compcert verified compiler                         *)
-(*                                                                     *)
-(*          Xavier Leroy, INRIA Paris-Rocquencourt                     *)
-(*                                                                     *)
-(*  Copyright Institut National de Recherche en Informatique et en     *)
-(*  Automatique.  All rights reserved.  This file is distributed       *)
-(*  under the terms of the GNU General Public License as published by  *)
-(*  the Free Software Foundation, either version 2 of the License, or  *)
-(*  (at your option) any later version.  This file is also distributed *)
-(*  under the terms of the INRIA Non-Commercial License Agreement.     *)
-(*                                                                     *)
-(* *********************************************************************)
-
-(* TL TODO: this is a fragment of original file, don't know how to handle licencing properly **)
 
 (* Library of useful Caml <-> Coq conversions *)
-
 
 (* Strings *)
 
