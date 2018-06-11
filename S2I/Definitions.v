69a5e1b831cde854ff261ca75aecc31b59883859
Rob Blanco <roberto.blanco@inria.fr>
2018-06-11 20:53:16 +0200

    Move matching_mains to S2I, prove transitivity
    Transitivity, in particular, from S to I, and then between I and I.
    Unfortunately, working with two separate definitions.
    
    The new location is not perfect, but it begins to untangle the presence
    of bouth Source and Intermediate in Source.Definability.

--- S2I/Definitions.v
+++ S2I/Definitions.v
@@ -22,3 +22,26 @@ End I.
 
 Ltac simplify_turn :=
   S.PS.simplify_turn; I.PS.simplify_turn.
+
+(* RB: The following should go into its own file. *)
+Definition matching_mains (p1 : Language.Source.program)
+                          (p2 : Machine.Intermediate.program) : Prop :=
+  Language.Source.prog_main p1 = None <->
+  Machine.Intermediate.prog_main p2 = None.
+
+Require Import Intermediate.Machine.
+Lemma matching_mains_trans : forall p1 p2 p3,
+  matching_mains p1 p2 ->
+  Intermediate.matching_mains p2 p3 ->
+  matching_mains p1 p3.
+Proof.
+  unfold matching_mains, Intermediate.matching_mains.
+  intros p1 p2 p3 H12 H23.
+  split; intros HNone.
+  - apply H12 in HNone.
+    apply H23 in HNone.
+    exact HNone.
+  - apply H23 in HNone.
+    apply H12 in HNone.
+    exact HNone.
+Qed.
