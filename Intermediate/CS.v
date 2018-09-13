a1ac000bef64ba83a6f1a9a9db4f8ce090609e12
Rob Blanco <roberto.blanco@inria.fr>
2018-09-13 12:42:11 +0200

    Add interface to execution from initial state
    This is likely to be the main missing piece of information to move
    forward. It is also a breaking change: corrections will be introduced
    gradually in the affected modules.

--- Intermediate/CS.v
+++ Intermediate/CS.v
@@ -836,8 +836,9 @@ Qed.
 
 End Semantics.
 
-Definition comes_from_initial_state (s: state) : Prop :=
+Definition comes_from_initial_state (s: state) (iface : Program.interface) : Prop :=
   exists p s0 t,
+    prog_interface p = iface /\
     initial_state p s0 /\
     Star (sem p) s0 t s.
 
