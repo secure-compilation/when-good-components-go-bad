c6741d8dcca39a96cc8e3d4ccf83d9180a55f3b6
Rob Blanco <roberto.blanco@inria.fr>
2018-09-26 20:17:08 -0500

    Another ProgCtxSim case, another tentative proof
    As we note in the comments, the simple proof script that works in the
    first few cases with very small changes, used without further
    adjustments, leads to some sub-goals that are not immediately solvable
    from the context as in previous cases.
    
    A possible alternative explored here purports to use the information in
    the context, notably provenance and stepping hypotheses, to move
    
     - from a CS.step on the program, p, and a context, p', that is
       compatible with and replaces c,
    
     - to a new CS.step on the program, p, and the original context, c,
       built based on the state before the step and on the step proper,
       which is on p's domain.
    
    From this, the proof for the ILoad case is simplified in various ways
    and avoids sub-goals where the solution is not straightforward.

--- Intermediate/Composition.v
+++ Intermediate/Composition.v
@@ -1306,6 +1306,85 @@ Section Simulation.
           -- reflexivity.
     }
     (* RB: TODO: A couple of cases dealing with memories, etc.; then compose. *)
+
+    (* The next few cases admit the same pattern as above, sometimes with very
+       minor generalizations. The ones that follow, then, are more interesting
+       as the first pattern finds problematic sub-goals in its present form. *)
+
+    5:{
+    (* - (* ILoad *) *)
+      assert (Hstep_cs' : CS.step (prepare_global_env (program_link p c))
+                                  (gps, mem, regs, pc) E0
+                                  (gps, mem, Register.set r2 v regs, Pointer.inc pc))
+        by admit.
+      match goal with
+      | |- context[PS.CC (?C, ?GPS, ?MEM)] =>
+        exists (PS.CC (C, GPS, MEM))
+      end.
+      split.
+      + constructor.
+        * easy.
+        * apply Hcc2.
+        * pose proof PS.context_epsilon_step_is_silent. econstructor.
+          -- reflexivity.
+          -- assumption.
+          -- assumption.
+          -- eapply linkable_sym; eassumption.
+          -- exact (linkable_mains_sym main_linkability).
+          -- rewrite program_linkC.
+             apply Hstep_cs'.
+             ++ assumption.
+             ++ assumption.
+             ++ apply linkable_sym; assumption.
+          -- assumption.
+          -- pose proof PS.partialized_state_is_partial
+                  (gps, mem, Register.set r2 v regs, Pointer.inc pc)
+                  (prog_interface p)
+               as Hpartial''.
+             assert (Htmp : Pointer.component (Pointer.inc pc) \in domm (prog_interface p))
+               by admit.
+             unfold PS.partialize in Hpartial''.
+             rewrite Htmp in Hpartial''.
+             rewrite Pointer.inc_preserves_component in Hpartial''.
+             assumption.
+      + rewrite <- Hmem.
+        rewrite <- Hstk.
+        match goal with
+        | |- PS.mergeable_states _ _ ?PC ?CC =>
+          eapply PS.mergeable_states_intro
+            with (ics := PS.unpartialize (PS.merge_partial_states PC CC))
+        end.
+        * easy.
+        * simpl.
+          rewrite (merge_stacks_partition Hmerge_iface Hfrom_initial).
+          rewrite (merge_memories_partition Hmerge_iface Hfrom_initial).
+          {
+            (* RB: TODO: Refactor into lemma (cf. other cases). *)
+            inversion mergeable_interfaces as [[_ Hdisjoint] _]. rewrite fdisjointC in Hdisjoint.
+            rewrite (unionmC Hdisjoint) in Hfrom_initial.
+            rewrite (unionmC Hdisjoint).
+            eapply (PS.comes_from_initial_state_step Hfrom_initial).
+            unfold PS.partialize.
+            apply PS.notin_to_in_false in Hpc1. rewrite Hpc1.
+            apply PS.notin_to_in_false in Hpc1'. rewrite Hpc1'.
+            rewrite Hmem. rewrite Hmem in Hstep_ps.
+            rewrite Hstk. rewrite Hstk in Hstep_ps.
+            exact Hstep_ps.
+          }
+        * constructor.
+          -- assumption.
+          -- by rewrite (merge_memories_partition Hmerge_iface Hfrom_initial).
+          -- by rewrite (merge_stacks_partition Hmerge_iface Hfrom_initial).
+        * simpl.
+          rewrite (merge_memories_partition Hmerge_iface Hfrom_initial).
+          rewrite (merge_stacks_partition Hmerge_iface Hfrom_initial).
+          rewrite <- Pointer.inc_preserves_component.
+          constructor.
+          -- by rewrite Pointer.inc_preserves_component.
+          -- reflexivity.
+          -- reflexivity.
+    }
+
   Admitted.
 
   Theorem context_simulates_program:
