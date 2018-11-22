d7089ddc5534f77ed5de3409d8786d489216b343
Rob Blanco <roberto.blanco@inria.fr>
2018-11-22 16:01:37 -0500

    prog_main_block is zero when main is not present
    And we trigger potentially buggy behavior in Coq for free!

--- Intermediate/CS.v
+++ Intermediate/CS.v
@@ -71,6 +71,26 @@ Definition prog_main_block (p : program) : Block.id :=
   | None => 0
   end.
 
+Lemma prog_main_block_no_main:
+  forall p,
+    well_formed_program p ->
+    Component.main \notin domm (prog_interface p) ->
+    CS.prog_main_block p = 0.
+Proof.
+  intros p Hwf Hdomm.
+  unfold CS.prog_main_block. (* Enable automatic rewrite on destruct. *)
+  destruct (prog_main p) as [main |] eqn:Hmain'.
+  - (* RB: TODO: Report bug.
+       Below, replacing BUGGY with an anonymous pattern triggers:
+       Error: Anomaly "make_elim_branch_assumptions." Please report at http://coq.inria.fr/bugs/.
+       This does not happen in other places. *)
+    inversion Hwf as [BUGGY _ _ _ _ _ [_ Hcontra]]; clear BUGGY.
+    rewrite Hmain' in Hcontra. specialize (Hcontra (eq_refl _)).
+    rewrite Hcontra in Hdomm.
+    discriminate.
+  - reflexivity.
+Qed.
+
 Theorem initial_machine_state_after_linking:
   forall p c,
     well_formed_program p ->
