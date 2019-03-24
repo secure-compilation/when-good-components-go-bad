d0019336a901b5f14a61550cc5c88af9a5a5ca0e
Rob Blanco <roberto.blanco@inria.fr>
2019-03-24 21:36:10 +0100

    Rephrase top-level lemmas for recombination
    The more abstract components of a proof of recombination (decomposition
    + composition) are modeled after the proof of recomposition, expected to
    be a close relative of it. For now the same names are kept even as in
    this new setting some should later find more apt designations.
    
    Importantly, the notion of compatibility between states is no longer
    given by the mergeability between two PS states (with all the
    complications that entailed), but is stated in terms of a relation
    between two CS states and a function that performs "state
    recombination", both as yet unspecified. Together with these must come a
    theorem relating them, i.e., nothing that whenever the relation is
    satisfied by a pair of states, the function will succeed in producing a
    result. We shall use option types to keep the obligations lightweight
    while explicitly covering nonsensical conditions, and ruling these out
    with the help of the relation.

--- Intermediate/Recombination.v
+++ Intermediate/Recombination.v
@@ -25,6 +25,31 @@ Set Bullet Behavior "Strict Subproofs".
 
 Import Intermediate.
 
+Section BehaviorStar.
+  Variables p c: program.
+
+  Hypothesis wf1 : well_formed_program p.
+  Hypothesis wf2 : well_formed_program c.
+
+  Hypothesis mergeable_interfaces:
+    mergeable_interfaces (prog_interface p) (prog_interface c).
+
+  (* Hypothesis linkability: linkable (prog_interface p) (prog_interface c). *)
+  Hypothesis main_linkability: linkable_mains p c.
+
+  Hypothesis prog_is_closed:
+    closed_program (program_link p c).
+
+  (* RB: Could be phrased in terms of does_prefix. *)
+  Theorem behavior_prefix_star b m :
+    program_behaves (CS.sem (program_link p c)) b ->
+    prefix m b ->
+  exists s1 s2,
+    CS.initial_state (program_link p c) s1 /\
+    Star (CS.sem (program_link p c)) s1 (finpref_trace m) s2.
+  Admitted.
+End BehaviorStar.
+
 Section Recombination.
   Variables p c p' c' : program.
 
@@ -48,6 +73,36 @@ Section Recombination.
 
   Hypothesis Hprog_is_closed  : closed_program (program_link p  c ).
   Hypothesis Hprog_is_closed' : closed_program (program_link p' c').
+
+  Inductive mergeable_states (iface1 iface2 : Program.interface) (s1 s2 : CS.state) : Prop.
+
+  Definition merge_states (s1 s2 : CS.state) : option CS.state :=
+    None.
+
+  Theorem initial_states_mergeability s s'' :
+    initial_state (CS.sem (program_link p  c )) s   ->
+    initial_state (CS.sem (program_link p' c')) s'' ->
+    mergeable_states ip ic s s''.
+  Admitted.
+
+  Lemma initial_state_merge_after_linking s s'' :
+    initial_state (CS.sem (program_link p  c )) s   ->
+    initial_state (CS.sem (program_link p' c')) s'' ->
+  exists s',
+    merge_states s s'' = Some s' /\
+    initial_state (CS.sem (program_link p  c')) s'.
+  Admitted.
+
+  Theorem threeway_multisem_star_simulation s1 s1'' t s2 s2'' :
+    mergeable_states ip ic s1 s1'' ->
+    Star (CS.sem (program_link p  c )) s1   t s2   ->
+    Star (CS.sem (program_link p' c')) s1'' t s2'' ->
+  exists s1' s2',
+    merge_states s1 s1'' = Some s1' /\
+    merge_states s2 s2'' = Some s2' /\
+    Star (CS.sem (program_link p  c')) s1' t s2'.
+    (* /\ mergeable_states ip ic s2 s2'' *)
+  Admitted.
 
   Theorem recombination_prefix m :
     does_prefix (CS.sem (program_link p  c )) m ->
