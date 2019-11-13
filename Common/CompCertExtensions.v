b0407b942df0e55a1d04a1f3679c5ff890118b9f
Rob Blanco <roberto.blanco@inria.fr>
2019-11-13 09:27:23 +0100

    Canonical structure for extended events
    Extend the embedding of events in (now nested) sum types and restore
    the declaration of the canonical structure for this embedding. In doing
    this, we follow FG's development, and declare a canonical structure for
    the equality of values, which was not present in the master until now.
    
    A question remains of whether to consider a pair of "undefined" value
    constructors equal or not. Semantically, this may well not be the case,
    but we may be getting ahead of ourselves by introducing this semantic
    notion early on.
    
    Co-authored-by: Akram El-Korashy <elkorashy@mpi-sws.org>

--- Common/CompCertExtensions.v
+++ Common/CompCertExtensions.v
@@ -1,4 +1,5 @@
 Require Import Common.Definitions.
+Require Import Common.Values.
 Require Import CompCert.Events.
 Require Import CompCert.Smallstep.
 Require Import CompCert.Behaviors.
@@ -10,13 +11,15 @@ From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat eqtype seq.
    can extend the encoding by using nested sums. *)
 Definition sum_of_event (e : event) :=
   match e with
-  | ERet C v C' => inl (C, v, C')
-  | ECall C P v C' => inr (C, P, v, C')
+  | ERet C v C' => inl (inl (C, v, C'))
+  | ECall C P v C' => inl (inr (C, P, v, C'))
+  | ERead C p v => inr (inl (C, p, v))
+  | EWrite C p v => inr (inr (C, p, v))
   end.
 
 Lemma sum_of_event_inj : injective sum_of_event.
 Proof.
-by case=> [????|???] [????|???] //= => [[-> -> -> ->]|[-> -> ->]].
+by case=> [????|???|???|???] [????|???|???|???] //= => [[-> -> -> ->]|[-> -> ->]|[-> -> ->]|[-> -> ->]].
 Qed.
 
 Definition event_eqMixin := InjEqMixin sum_of_event_inj.
