dfe5eb5c7d797ae20cbbc23848ba26ba1f539bd1
Akram El-Korashy <elkorashy@mpi-sws.org>
2020-06-08 11:04:31 +0200

    Start a copy of events and traces (for the informative traces)

---
+++ Common/TracesInform.v
@@ -0,0 +1,195 @@
+Require Import Lib.Extra.
+Require Import CompCert.Events.
+Require Import Common.Definitions.
+Require Import Common.Linking.
+Require Import Common.CompCertExtensions.
+
+From mathcomp
+  Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+(* The following section will need to be refactored out from both this file
+   and from Traces.v.
+*)
+Section Suffix.
+
+Variable T : eqType.
+
+Implicit Types (x : T) (s : seq T).
+
+Fixpoint suffix_rec s1 s2 : bool :=
+  (s1 == s2) ||
+  if s2 is x :: s2' then suffix_rec s1 s2' else false.
+
+Definition suffix := locked suffix_rec.
+
+Lemma suffix_refl : reflexive suffix.
+Proof. by case=> [|??]; rewrite /suffix -lock //= eqxx. Qed.
+
+Lemma suffix_cons s1 x s2 :
+  suffix s1 (x :: s2) = (s1 == x :: s2) || suffix s1 s2.
+Proof. by rewrite /suffix; unlock. Qed.
+
+Lemma suffix_nil s1 : suffix s1 [::] = (s1 == [::]).
+Proof. by rewrite /suffix -lock /= orbF. Qed.
+
+End Suffix.
+
+Section Traces.
+
+Inductive stack_state := StackState {
+  (* The current running component.  *)
+  cur_comp : Component.id;
+
+  (* Other running components further down the stack. *)
+  callers  : list Component.id
+}.
+
+Definition stack_state0 := StackState Component.main [::].
+
+Implicit Types (t : trace) (C : Component.id) (s : stack_state) (intf : Program.interface).
+
+Fixpoint well_bracketed_trace s t : bool :=
+  match t with
+  | [::] => true
+  | e :: t' =>
+    (cur_comp s == cur_comp_of_event e) &&
+    match e with
+    | ECall C _ _ C' =>
+      well_bracketed_trace (StackState C' (C :: callers s)) t'
+    | ERet C _ C' =>
+      match callers s with
+      | [] => false
+      | head :: tail =>
+        (head == C') && well_bracketed_trace (StackState C' tail) t'
+      end
+    | _ => well_bracketed_trace s t'
+    end
+  end.
+
+Definition run_event s e :=
+  match e with
+  | ECall C _ _ C' => StackState C' (C :: callers s)
+  | ERet  C _   C' => StackState C' (tail (callers s))
+  | _ => s
+  end.
+
+Definition run_trace s t := foldl run_event s t.
+
+Lemma run_trace1 s t e : run_trace s (rcons t e) = run_event (run_trace s t) e.
+Proof. by rewrite -cats1 /run_trace foldl_cat. Qed.
+
+Lemma well_bracketed_trace_cat s t1 t2 :
+  well_bracketed_trace s (t1 ++ t2) =
+  well_bracketed_trace s t1 &&
+  well_bracketed_trace (run_trace s t1) t2.
+Proof.  
+(* TODO: Try to understand and fix the proof. *)
+elim: t1 s=> [//|[C ? ? C'|C ? C'|? ? ?|? ? ?] t1 IH] [Ccur callers] /=.
+  by rewrite IH andbA.
+case: eqP callers => [_ {Ccur}|_] //= [|top callers] //=.
+  by rewrite IH andbA.
+Admitted.
+
+Definition seq_of_stack_state s := cur_comp s :: callers s.
+
+Coercion seq_of_stack_state : stack_state >-> seq.
+
+Lemma seq_of_stack_state_inj : injective seq_of_stack_state.
+Proof. by case=> [??] [??] [-> ->]. Qed.
+
+Lemma well_bracketed_trace_suffix t C C' Cs :
+  well_bracketed_trace stack_state0 t ->
+  suffix [:: C, C' & Cs] (run_trace stack_state0 t) ->
+  exists t1 P arg t2, t = t1 ++ ECall C' P arg C :: t2.
+Proof.
+(* TODO: Try to understand and fix the proof. *)
+set s0 := stack_state0.
+elim/last_ind: t=> [|t e IH] //=.
+  by rewrite suffix_cons suffix_nil /= orbF => _ /eqP.
+have -> : well_bracketed_trace s0 (rcons t e) =
+          well_bracketed_trace s0 t &&
+          well_bracketed_trace (run_trace s0 t) [:: e].
+  by rewrite -cats1 well_bracketed_trace_cat andbC.
+rewrite run_trace1; case: e => [C1 P arg C2|C1 ? C2|? ? ?|? ? ?] /=.
+  rewrite andbT; case/andP=> wb_t /eqP <- {C1} /=.
+  rewrite -[_ :: callers _]/(run_trace s0 t : seq _) suffix_cons /=.
+  case/orP=> [/eqP|Hsuff].
+    case: (run_trace _ _)=> [? ?] [<- <- <-] /=.
+    by eexists t, _, _, [::]; rewrite cats1.
+  case/(_ wb_t Hsuff): IH=> [t1 [P' [arg' [t2 ->]]]].
+  by eexists t1, P', arg', (rcons t2 _); rewrite rcons_cat; split; eauto.
+case/and3P=> wb_t /eqP e1 e2.
+have -> : StackState C2 (tl (callers (run_trace s0 t))) =
+          callers (run_trace s0 t) :> seq _.
+  by case: (callers _) e2=> [|e cs] //= /andP [/eqP ->].
+move=> Hsuff; have {Hsuff} Hsuff: suffix [:: C, C' & Cs] (run_trace s0 t).
+  by rewrite suffix_cons Hsuff orbT.
+case/(_ wb_t Hsuff): IH=> [t1 [P [arg [t2 ->]]]].
+  by eexists t1, P, arg, (rcons _ _); rewrite rcons_cat.
+Admitted.
+
+Lemma well_bracketed_trace_inv t C res C' :
+  well_bracketed_trace stack_state0 (t ++ [:: ERet C res C']) ->
+  exists t1 P arg t2, t = t1 ++ ECall C' P arg C :: t2.
+Proof.
+rewrite -[t]cat0s.
+elim: t {1 3}nil=> [|e t IH] pre //=; last first.
+  rewrite -cat1s [pre ++ _]catA; exact: IH.
+rewrite cats0 well_bracketed_trace_cat /=.
+case/and3P=> wb_t /eqP e_C e_C'.
+have : suffix [:: C, C' & tail (callers (run_trace stack_state0 pre))]
+              (run_trace stack_state0 pre).
+  case: run_trace e_C e_C'=> [? [|??]] //=; rewrite andbT.
+  by move=> -> /eqP ->; rewrite suffix_refl.
+exact: well_bracketed_trace_suffix=> //.
+Qed.
+
+(* TODO: Here, this is an important definition.
+   Need as an extra argument the status of the (shared?) memory at the program state in which
+   the event was emitted.
+   Only then can we use reachability to compute the views of each component.
+   Based on the view of a component memory, we can judge whether an ERead/EWrite that it
+   performs is a possible read/write.  *)
+Definition well_formed_event intf (e: event) : bool :=
+  match e with
+  | ECall C P _ C' => (C != C') && imported_procedure_b intf C C' P
+  | ERet  C _   C' => (C != C')
+  | ERead _ _ _ => false
+  | EWrite _ _ _ => false
+  end.
+
+Definition well_formed_trace intf (t: trace) : bool :=
+  well_bracketed_trace stack_state0 t &&
+  all (well_formed_event intf) t.
+
+Definition declared_event_comps intf e :=
+  [&& cur_comp_of_event e \in domm intf &
+      next_comp_of_event e \in domm intf].
+
+Lemma well_formed_trace_int intf t :
+  well_formed_trace intf t ->
+  closed_interface intf ->
+  all (declared_event_comps intf) t.
+Proof.
+Admitted.
+(*
+case/andP=> wb wf clos; rewrite /declared_event_comps.
+apply/allP; case=> [C P v C'|C v C'] /=; rewrite !mem_domm.
+- move/(allP wf)=> /andP [_ imp].
+  move: (imp); rewrite /imported_procedure_b; case: getm => //= _ _.
+  by case/imported_procedure_iff/clos: imp=> ? [->].
+- move=> in_p; case/path.splitP: in_p wb wf => {t} t1 t2.
+  rewrite -cats1 /= well_bracketed_trace_cat.
+  case/andP=> /well_bracketed_trace_inv [t11 [P [v' [t12 ->]]]] _ wf.
+  have : well_formed_event intf (ECall C' P v' C).
+    by move/allP: wf; apply; rewrite !mem_cat inE eqxx /= orbT.
+  case/andP=> _ imp.
+  case/imported_procedure_iff/clos: (imp)=> ? [-> _] /=.
+  by move: imp; rewrite /imported_procedure_b; case: getm.
+Qed.
+*)
+End Traces.
---
+++ CompCert/EventsInform.v
@@ -0,0 +1,241 @@
+Require Import Common.Definitions.
+Require Import Common.Values.
+Require Import CompCert.Coqlib.
+
+(* RB: TODO: RW: Allowing arbitrary values to appear in events does not preclude
+   the possibility of nonsensical values appearing in traces, notably Undef
+   values. We have two basic alternatives:
+    1. Rule out Undef values in the semantics.
+    2. Refine the definition of defined and undefined values at the type level.
+*)
+Inductive event :=
+| ECall : Component.id -> Procedure.id -> Z (* value *) -> Component.id -> event
+| ERet : Component.id -> Z (* value *) -> Component.id -> event
+| ERead : Component.id -> Pointer.t -> value -> event
+| EWrite : Component.id -> Pointer.t -> value -> event.
+
+Definition cur_comp_of_event (e: event) : Component.id :=
+  match e with
+  | ECall  C _ _ _ => C
+  | ERet   C _ _   => C
+  | ERead  C _ _   => C
+  | EWrite C _ _   => C
+  end.
+
+Definition next_comp_of_event (e: event) : Component.id :=
+  match e with
+  (* Calls and returns yield control. *)
+  | ECall  _ _ _ C => C
+  | ERet   _ _   C => C
+  (* Reads and writes retain control. *)
+  | ERead  C _ _   => C
+  | EWrite C _ _   => C
+  end.
+
+Definition trace := list event.
+
+Definition E0 : trace := nil.
+
+Definition Eapp (t1 t2: trace) : trace := t1 ++ t2.
+
+CoInductive traceinf : Type :=
+  | Econsinf: event -> traceinf -> traceinf.
+
+Fixpoint Eappinf (t: trace) (T: traceinf) {struct t} : traceinf :=
+  match t with
+  | nil => T
+  | ev :: t' => Econsinf ev (Eappinf t' T)
+  end.
+
+(** Concatenation of traces is written [**] in the finite case
+  or [***] in the infinite case. *)
+
+Infix "**" := Eapp (at level 60, right associativity).
+Infix "***" := Eappinf (at level 60, right associativity).
+
+Lemma E0_left: forall t, E0 ** t = t.
+Proof. auto. Qed.
+
+Lemma E0_right: forall t, t ** E0 = t.
+Proof. intros. unfold E0, Eapp. rewrite <- app_nil_end. auto. Qed.
+
+Lemma Eapp_assoc: forall t1 t2 t3, (t1 ** t2) ** t3 = t1 ** (t2 ** t3).
+Proof. intros. unfold Eapp, trace. apply app_ass. Qed.
+
+Lemma Eapp_E0_inv: forall t1 t2, t1 ** t2 = E0 -> t1 = E0 /\ t2 = E0.
+Proof.
+  intros. apply app_eq_nil. auto.
+Qed.
+
+Lemma E0_left_inf: forall T, E0 *** T = T.
+Proof. auto. Qed.
+
+Lemma Eappinf_assoc: forall t1 t2 T, (t1 ** t2) *** T = t1 *** (t2 *** T).
+Proof.
+  induction t1; intros; simpl. auto. decEq; auto.
+Qed.
+
+Hint Rewrite E0_left E0_right Eapp_assoc
+             E0_left_inf Eappinf_assoc: trace_rewrite.
+
+Opaque trace E0 Eapp Eappinf.
+
+(** The following [traceEq] tactic proves equalities between traces
+  or infinite traces. *)
+
+Ltac substTraceHyp :=
+  match goal with
+  | [ H: (@eq trace ?x ?y) |- _ ] =>
+       subst x || clear H
+  end.
+
+Ltac decomposeTraceEq :=
+  match goal with
+  | [ |- (_ ** _) = (_ ** _) ] =>
+      apply (f_equal2 Eapp); auto; decomposeTraceEq
+  | _ =>
+      auto
+  end.
+
+Ltac traceEq :=
+  repeat substTraceHyp; autorewrite with trace_rewrite; decomposeTraceEq.
+
+(** Bisimilarity between infinite traces. *)
+
+CoInductive traceinf_sim: traceinf -> traceinf -> Prop :=
+  | traceinf_sim_cons: forall e T1 T2,
+      traceinf_sim T1 T2 ->
+      traceinf_sim (Econsinf e T1) (Econsinf e T2).
+
+Lemma traceinf_sim_refl:
+  forall T, traceinf_sim T T.
+Proof.
+  cofix COINDHYP; intros.
+  destruct T. constructor. apply COINDHYP.
+Qed.
+
+Lemma traceinf_sim_sym:
+  forall T1 T2, traceinf_sim T1 T2 -> traceinf_sim T2 T1.
+Proof.
+  cofix COINDHYP; intros. inv H; constructor; auto.
+Qed.
+
+Lemma traceinf_sim_trans:
+  forall T1 T2 T3,
+  traceinf_sim T1 T2 -> traceinf_sim T2 T3 -> traceinf_sim T1 T3.
+Proof.
+  cofix COINDHYP;intros. inv H; inv H0; constructor; eauto.
+Qed.
+
+CoInductive traceinf_sim': traceinf -> traceinf -> Prop :=
+  | traceinf_sim'_cons: forall t T1 T2,
+      t <> E0 -> traceinf_sim' T1 T2 -> traceinf_sim' (t *** T1) (t *** T2).
+
+Lemma traceinf_sim'_sim:
+  forall T1 T2, traceinf_sim' T1 T2 -> traceinf_sim T1 T2.
+Proof.
+  cofix COINDHYP; intros. inv H.
+  destruct t. elim H0; auto.
+Transparent Eappinf.
+Transparent E0.
+  simpl.
+  destruct t. simpl. constructor. apply COINDHYP; auto.
+  constructor. apply COINDHYP.
+  constructor. unfold E0; congruence. auto.
+Qed.
+
+(** An alternate presentation of infinite traces as
+  infinite concatenations of nonempty finite traces. *)
+
+CoInductive traceinf': Type :=
+  | Econsinf': forall (t: trace) (T: traceinf'), t <> E0 -> traceinf'.
+
+Program Definition split_traceinf' (t: trace) (T: traceinf') (NE: t <> E0): event * traceinf' :=
+  match t with
+  | nil => _
+  | e :: nil => (e, T)
+  | e :: t' => (e, Econsinf' t' T _)
+  end.
+Next Obligation.
+  elimtype False. elim NE. auto.
+Qed.
+Next Obligation.
+  red; intro. elim (H e). rewrite H0. auto.
+Qed.
+
+CoFixpoint traceinf_of_traceinf' (T': traceinf') : traceinf :=
+  match T' with
+  | Econsinf' t T'' NOTEMPTY =>
+      let (e, tl) := split_traceinf' t T'' NOTEMPTY in
+      Econsinf e (traceinf_of_traceinf' tl)
+  end.
+
+Remark unroll_traceinf':
+  forall T, T = match T with Econsinf' t T' NE => Econsinf' t T' NE end.
+Proof.
+  intros. destruct T; auto.
+Qed.
+
+Remark unroll_traceinf:
+  forall T, T = match T with Econsinf t T' => Econsinf t T' end.
+Proof.
+  intros. destruct T; auto.
+Qed.
+
+Lemma traceinf_traceinf'_app:
+  forall t T NE,
+  traceinf_of_traceinf' (Econsinf' t T NE) = t *** traceinf_of_traceinf' T.
+Proof.
+  induction t.
+  intros. elim NE. auto.
+  intros. simpl.
+  rewrite (unroll_traceinf (traceinf_of_traceinf' (Econsinf' (a :: t) T NE))).
+  simpl. destruct t. auto.
+Transparent Eappinf.
+  simpl. f_equal. apply IHt.
+Qed.
+
+(** Prefixes of traces. *)
+
+Definition trace_prefix (t1 t2: trace) :=
+  exists t3, t2 = t1 ** t3.
+
+Definition traceinf_prefix (t1: trace) (T2: traceinf) :=
+  exists T3, T2 = t1 *** T3.
+
+Lemma trace_prefix_app:
+  forall t1 t2 t,
+  trace_prefix t1 t2 ->
+  trace_prefix (t ** t1) (t ** t2).
+Proof.
+  intros. destruct H as [t3 EQ]. exists t3. traceEq.
+Qed.
+
+Lemma traceinf_prefix_app:
+  forall t1 T2 t,
+  traceinf_prefix t1 T2 ->
+  traceinf_prefix (t ** t1) (t *** T2).
+Proof.
+  intros. destruct H as [T3 EQ]. exists T3. subst T2. traceEq.
+Qed.
+
+Set Implicit Arguments.
+
+(* TODO: What does match_traces mean? How should it be updated now
+   that we have ERead and EWrite?
+ *)
+Inductive match_traces: trace -> trace -> Prop :=
+  | match_traces_E0:
+      match_traces nil nil
+  | match_traces_call: forall C P arg C',
+      match_traces (ECall C P arg C' :: nil)
+                   (ECall C P arg C' :: nil)
+  | match_traces_ret: forall C retval C',
+      match_traces (ERet C retval C' :: nil)
+                   (ERet C retval C' :: nil)
+  | match_traces_read: forall C ptr v,
+      match_traces (ERead C ptr v :: nil)
+                   (ERead C ptr v :: nil)
+  | match_traces_write: forall C ptr v,
+      match_traces (EWrite C ptr v :: nil)
+                   (EWrite C ptr v :: nil).
