d23b187ea63638e68dc80d96205dd4944e6fdd07
Akram El-Korashy <elkorashy@mpi-sws.org>
2022-02-02 16:24:40 +0100

    Try to evaluate prog_of_trace but get a partially evaluated and huge term for a tiny input

---
+++ Examples.v
@@ -0,0 +1,60 @@
+Require Import CompCert.Behaviors.
+Require Import CompCert.Smallstep.
+Require Import Common.Definitions.
+Require Import Common.Linking.
+Require Import Common.CompCertExtensions.
+Require Import Common.TracesInform.
+Require Import Common.RenamingOption.
+Require Import Common.Memory.
+
+Require Import Source.DefinabilityEnd.
+Require Import Source.Language.
+Require Import Source.GlobalEnv.
+Require Import Source.CS.
+Require Import Intermediate.Machine.
+Require Import Intermediate.CS.
+Require Import Intermediate.RecombinationRel.
+Require Import S2I.Compiler.
+Require Import S2I.Definitions.
+
+
+From mathcomp Require Import ssreflect ssrfun ssrbool.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+
+Set Bullet Behavior "Strict Subproofs".
+
+
+Definition c1 : Component.id := 1.
+Definition c1p1 : Procedure.id := 1.
+Definition c1exports : {fset Procedure.id} := fset1 c1p1.
+Definition c1imports : {fset Component.id * Procedure.id} := fset0.
+
+Definition c1intf : Component.interface.
+  constructor; [exact c1exports | exact c1imports].
+Defined.
+
+Definition Pintf : Program.interface :=
+  setm emptym c1 c1intf.
+
+Definition prog_of_trace_Pintf := Definability.program_of_trace Pintf.
+
+Definition Pbuffers : NMap (nat + list value) :=
+  setm emptym c1 (inr [Int 50; Int 60; Int 70]).
+
+Definition c1mem :=
+  ComponentMemory.prealloc Pbuffers.
+
+Definition Pmem := setm emptym c1 c1mem.
+
+Definition prog_of_trace_Pintf_Pbuffers := prog_of_trace_Pintf Pbuffers.
+
+Definition tP : Events.trace event_inform.
+  apply cons.
+  - apply EConst.
+    + exact c1.
+    + exact (Int 700).
+    + exact E_R_ONE.
+    + exact (setm emptym c1 c1mem).
+    + 
