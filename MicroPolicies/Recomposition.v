7bb780508842071a5c4f3d314155c06cf0834e4d
Basile Schlosser <basile.schlosser@ens-paris-saclay.fr>
2025-04-10 18:24:13 +0200

    Fleshed out the definitions in Recomposition, proved determinacy of the semantics and layed some groundwork for the proofs of the 2 main recomposition properties.

--- MicroPolicies/Recomposition.v
+++ MicroPolicies/Recomposition.v
@@ -26,6 +26,9 @@ Require Import MicroPolicies.LRC.
 Require Import MicroPolicies.Merged.
 Require Import Coq.micromega.Lia.
 
+Require Import MicroPolicies.Utils.
+Import DoNotation.
+
 Record program :=
   { prog_interface: Program.interface;
     code: @Merged.code concrete_int_32_mt;
@@ -40,14 +43,70 @@ Definition program_link p c :=
     prog_main := prog_main p || prog_main c;
   |}.
 
+(* definition taken from Source/Language.v *)
+
+Record closed_program (p: program) := {
+    (* the interface must be closed (and consequently sound) *)
+    cprog_closed_interface:
+    closed_interface (prog_interface p);
+    (* the main procedure must exist *)
+    cprog_main_existence: prog_main p
+  }.
+
+
+(* Component C has a buffer of size at least one *)
+Definition has_required_local_buffers (p: program) (C: Component.id) : Prop :=
+  exists (b: nat),
+    match (Option.bind (fun (r: {fmap Block.id -> nat + seq value}) => r b ) (prog_buffers p C)) with
+    | None => False
+    | Some buf =>
+        match buf with
+        | inl size   => size > 0
+        | inr values => length values > 0
+        end%nat
+    end.
+
+Definition get_procedures p :=
+  fset (map (fun '(_, tag) => color tag) (code p)).
+
+Definition find_procedure p comp proc :=
+  Exists (fun '(_, tag) =>
+            match (entry tag) with
+            | None => False
+            | Some (proc', lc) => (proc = proc') /\ (In comp lc)
+            end) (code p).
+
+Record well_formed_program (p: program) := {
+    (* the interface is sound (but maybe not closed) *)
+    wfprog_interface_soundness:
+    sound_interface (prog_interface p);
+    (* there are procedures only for the declared components *)
+    wfprog_defined_procedures: domm (prog_interface p) = get_procedures p;
+    (* each exported procedure is actually defined *)
+    wfprog_exported_procedures_existence: 
+    forall C P, exported_procedure (prog_interface p) C P ->
+           find_procedure p C P;
+    (* each declared component has the required static buffers *)
+    wfprog_defined_buffers: domm (prog_interface p) = domm (prog_buffers p);
+    (* each component's buffer is well formed *)
+    wfprog_well_formed_buffers:
+    forall C, prog_interface p C ->
+         has_required_local_buffers p C;
+    (* iff the main component is defined, so is the main procedure
+       RB: Changed from a simple conditional. *)
+    wfprog_main_existence:
+    Component.main \in domm (prog_interface p) <-> prog_main p
+  }.
+
+
 (* Main simulation theorem. *)
 Section Recombination.
   Variables p c p' c' : program.
 
-  (* Hypothesis Hwfp  : well_formed_program p. *)
-  (* Hypothesis Hwfc  : well_formed_program c. *)
-  (* Hypothesis Hwfp' : well_formed_program p'. *)
-  (* Hypothesis Hwfc' : well_formed_program c'. *)
+  Hypothesis Hwfp  : well_formed_program p. 
+  Hypothesis Hwfc  : well_formed_program c. 
+  Hypothesis Hwfp' : well_formed_program p'.
+  Hypothesis Hwfc' : well_formed_program c'.
 
   Hypothesis Hmergeable_ifaces :
     mergeable_interfaces (prog_interface p) (prog_interface c).
@@ -55,8 +114,8 @@ Section Recombination.
   Hypothesis Hifacep  : prog_interface p  = prog_interface p'.
   Hypothesis Hifacec  : prog_interface c  = prog_interface c'.
 
-  (* Hypothesis Hprog_is_closed  : closed_program (program_link p  c ). *)
-  (* Hypothesis Hprog_is_closed' : closed_program (program_link p' c'). *)
+  Hypothesis Hprog_is_closed  : closed_program (program_link p  c ).
+  Hypothesis Hprog_is_closed' : closed_program (program_link p' c').
 
   Let ip := prog_interface p.
   Let ic := prog_interface c.
@@ -76,28 +135,47 @@ Section Recombination.
                                        | e :: [::] => step_mp s s' (Some e)
                                        | _ => False
                                        end).
-   Definition initial_state1: state -> Prop :=
-     fun s => s = initial_state (code prog  ) (prog_buffers prog  ) (prog_interface prog  ). 
+  Definition initial_state1: state -> Prop :=
+    fun s => s = initial_state (code prog  ) (prog_buffers prog  ) (prog_interface prog  ). 
 
-   Definition initial_state2: state -> Prop :=
-     fun s => s = initial_state (code prog' ) (prog_buffers prog' ) (prog_interface prog' ). 
+  Definition initial_state2: state -> Prop :=
+    fun s => s = initial_state (code prog' ) (prog_buffers prog' ) (prog_interface prog' ). 
 
-   Definition initial_state3: state -> Prop :=
-     fun s => s = initial_state (code prog'') (prog_buffers prog'') (prog_interface prog'').
-   
-  Variable final_state: state -> Prop. (*might be different between semantics*)
+  Definition initial_state3: state -> Prop :=
+    fun s => s = initial_state (code prog'') (prog_buffers prog'') (prog_interface prog'').
+  
+  Definition allowed_UB: state -> Prop :=
+    fun '(Symbolic.State m r (Types.Atom pc _) _ _) =>
+      match (m pc) with
+      | None => False
+      | Some (Types.Atom _ tag) => (color tag) \in (domm ic)
+      end.
 
-  Variable allowed_UB: state -> Prop.
+  Definition final_state_me: state -> Prop :=
+    fun '(Symbolic.State m r (Types.Atom pc _) _ _) =>
+      match (m pc) with
+      | None => False
+      | Some (Types.Atom i tag) =>
+          ((Types.decode_instr i) = Some (Types.Halt _)) /\ (is_code tag = true)
+      end.
+  
+  Definition final_state_rUB: state -> Prop :=
+    fun '(Symbolic.State m r (Types.Atom pc tpc) _ _) =>
+      match (m pc) with
+      | None => False
+      | Some (Types.Atom i tag) =>
+          ((Types.decode_instr i) = Some (Types.Halt _)) /\ ((color tag) \in (domm ip) -> (is_code tag = true))
+      end.
 
   Let sem   :=
-        L_restricted_UB step1 step2 initial_state1 final_state tt allowed_UB.
+        L_restricted_UB step1 step2 initial_state1 final_state_rUB tt allowed_UB.
 
   Let sem'  :=
         {| Smallstep.state := state;
           Smallstep.genvtype := genvtype;
           Smallstep.step := step1;
           Smallstep.initial_state := initial_state2;
-          Smallstep.final_state := final_state;
+          Smallstep.final_state := final_state_me;
           Smallstep.globalenv := tt |}.
   Let sem'' :=
 
@@ -105,10 +183,10 @@ Section Recombination.
           Smallstep.genvtype := genvtype;
           Smallstep.step := step1;
           Smallstep.initial_state := initial_state3;
-          Smallstep.final_state := final_state;
+          Smallstep.final_state := final_state_me;
           Smallstep.globalenv := tt |}.
   
-  Lemma det_L2 : determinate sem'.
+  Lemma det_sem' : determinate sem'.
   Proof.
     econstructor; intros; unfold sem'  in *.
     - simpl in *. unfold step1, step_me in *.
@@ -118,10 +196,13 @@ Section Recombination.
     - unfold single_events. intros.
       destruct t; try destruct t; try contradiction; simpl; lia.
     - simpl in *. unfold initial_state2 in *. subst. done.
-    - admit.
-  Admitted.
+    - simpl in *. intros t s' step. unfold step1, step_me in step. destruct t; try destruct t; try contradiction;
+        setoid_rewrite <- Exec.stepP in step; unfold Exec.stepf in step;
+        destruct s, pc0; unfold final_state_me in H; destruct (mem0 vala); try auto; destruct a;
+        destruct H as [eq ?]; rewrite eq in step; simpl in step; inversion step.
+  Qed.
   
-  Lemma det_L3 : determinate sem''.
+  Lemma det_sem'' : determinate sem''.
   Proof.
     econstructor; intros; unfold sem'  in *.
     - simpl in *. unfold step1, step_me in *.
@@ -131,17 +212,13 @@ Section Recombination.
     - unfold single_events. intros.
       destruct t; try destruct t; try contradiction; simpl; lia.
     - simpl in *. unfold initial_state3 in *. subst. done.
-    - admit.
-  Admitted.
+    - simpl in *. intros t s' step. unfold step1, step_me in step. destruct t; try destruct t; try contradiction;
+        setoid_rewrite <- Exec.stepP in step; unfold Exec.stepf in step;
+        destruct s, pc0; unfold final_state_me in H; destruct (mem0 vala); try auto; destruct a;
+        destruct H as [eq ?]; rewrite eq in step; simpl in step; inversion step.
+  Qed.
 
-  
-  Ltac left_subst :=
-    match goal with
-    | H : ?a = ?b, H0 : ?a = ?c |- _  =>
-        try (rewrite H0 in H; inv H)
-    end.
-  
-  Lemma det_L1 : determinate sem.
+  Lemma det_sem : determinate sem.
   Proof.
     econstructor; intros; unfold sem'  in *.
     - simpl in *. (*unfold step1, step2, step_mp, step_me in *. *)
@@ -149,26 +226,25 @@ Section Recombination.
         unfold step1, step2, step_mp, step_me in *;
         destruct t, t0; try destruct t; try destruct t0; try contradiction;
         (do 2 (match goal with | H : step _ _ _ _ |- _ => setoid_rewrite <- Exec.stepP in H end));
-        left_subst; try (split; [try destruct e; try econstructor|try done]).
+        (match goal with | H : ?a = ?b, H0 : ?a = ?c |- _  => try (rewrite H0 in H; inv H) end);
+        try (split; [try destruct e; try econstructor|try done]).
     - unfold single_events. intros.
       destruct H; unfold step1, step2, step_mp, step_me in *;
         destruct t; try destruct t; try contradiction;
         (match goal with | H : step _ _ _ _ |- _ => setoid_rewrite <- Exec.stepP in H end); simpl; lia.
     - simpl in *. unfold initial_state1 in *. subst. done.
-    - admit.
-  Admitted.
-  
-  Lemma single_L1: single_events sem.
-  Proof. eapply sd_traces. exact det_L1. Qed. 
-
-  Lemma single_L2: single_events sem'.
-  Proof. eapply sd_traces. exact det_L2. Qed. 
-
-  Lemma single_L3: single_events sem''.
-  Proof. eapply sd_traces. exact det_L3. Qed. 
+    - simpl in *. intros t s' step. destruct step; rename H0 into step; unfold step1, step2, step_mp, step_me in step;
+        destruct t; try destruct t; try contradiction;
+        setoid_rewrite <- Exec.stepP in step; unfold Exec.stepf in step; unfold final_state_me in H;
+        destruct s; unfold final_state_me in H ; simpl in H; destruct pc0;
+        remember (mem0 vala) as mv; unfold getm in H;
+        assert (eqmv: getm_def mem0 vala = mv) by ( clear - Heqmv; unfold getm in Heqmv; auto); rewrite eqmv in H;
+        destruct mv; try auto; try destruct a;
+        destruct H as [eq ?]; unfold Types.decode_instr in step; simpl in step; rewrite eq in step; simpl in step; inversion step.
+  Qed.
 
   Theorem simulation:
-    @threeway_simulation sem sem' sem'' single_L1 single_L2 single_L3.
+    @threeway_simulation sem sem' sem'' (sd_traces det_sem) (sd_traces det_sem') (sd_traces det_sem'').
   Proof.
     eapply threeway_simulation_diagram.
     - admit.
@@ -185,6 +261,28 @@ Section Recombination.
       does_prefix sem'  m ->
       does_prefix sem'' m.
   Proof.
+    setoid_rewrite (does_prefix_equiv det_sem).
+    setoid_rewrite (does_prefix_equiv det_sem').
+    setoid_rewrite (does_prefix_equiv det_sem'').
+    intros m dp_sem dp_sem'.
+    destruct simulation.
+    inversion dp_sem; subst; inversion dp_sem'; subst;
+      try (match goal with | H : (forall s : Smallstep.state _, ~ Smallstep.initial_state _ s) |- _ =>
+                               exfalso; eapply H; simpl; unfold initial_state2; unfold initial_state1; eauto end);
+      try (remember (initial_state (code prog'') (prog_buffers prog'') (prog_interface prog'')) as s3;
+           (assert (match_s3: match_states s s0 s3) by 
+             ( match goal with | H : (Smallstep.initial_state sem _), H' : (Smallstep.initial_state sem' _) |- _ =>
+               destruct props as [a]; destruct (a _ _ H H') as [s3' [init match_init]]; simpl in init; unfold initial_state3 in init;
+               rewrite <- init in Heqs3; subst; done end) );
+           (match goal with | H : (Star sem' _ _ _) |- _ =>
+           destruct (tsimulation_star props H0 H s3 match_s3) as [s3' [star_sem'' ?]] end);
+           assert (init: Smallstep.initial_state sem'' s3) by done).
+    - apply (does_FTbc (init) star_sem'').
+    - apply (does_FGoes_wrong (init) star_sem'').
+      + simpl. intros t' s'' step. admit. (* we should use one of the "diagram properties" here. *)
+      + admit. (* we should use properties of match_states and final_states here. *)
+    - apply (does_FTerminates (init) star_sem'').
+      admit. (* we should use properties of match_states and final_states here. *)
   Admitted.
 
   Corollary recomposition_blame':
@@ -193,6 +291,23 @@ Section Recombination.
       does_prefix sem'  (FTbc m) ->
       does_prefix sem'' (FGoes_wrong m).
   Proof.
+    setoid_rewrite (does_prefix_equiv det_sem).
+    setoid_rewrite (does_prefix_equiv det_sem').
+    setoid_rewrite (does_prefix_equiv det_sem'').
+    intros m dp_sem dp_sem'.
+    inversion dp_sem; inversion dp_sem';
+      try (match goal with | H : (forall s : Smallstep.state _, ~ Smallstep.initial_state _ s) |- _ =>
+                               exfalso; eapply H; simpl; unfold initial_state2; unfold initial_state1; eauto end).
+    destruct simulation. remember (initial_state (code prog'') (prog_buffers prog'') (prog_interface prog'')) as s3.
+    assert (match_s3: match_states s s0 s3).
+    { destruct props as [a]. destruct (a _ _ H0 H5) as [s3' [init match_init]]. simpl in init. unfold initial_state3 in init.
+      rewrite <- init in Heqs3. subst. done. }
+    destruct (tsimulation_star props H1 H6 s3 match_s3) as [s3' [star_sem'' ?]].
+    econstructor.
+    - simpl; unfold initial_state3; eauto.
+    - eauto. (* we might need to do a few more silent steps in s3'. *)
+    - simpl. intros t' s'' step. admit. (* we should use one of the "diagram properties" here. *)
+    - admit. (* we should use properties of match_states and final_states here. *)
   Admitted.
 
 
